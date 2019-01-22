fdb36c75b0b3a2898234d56429c375e9dbf1e584
Stepan <stepos36@gmail.com>
2019-01-21 19:49:37 -0500

    initial app files

---
+++ liri.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var command = process.argv[2];
+var request = process.argv[3]
+if (command==="concert-this") {
+
+}
+if (command==="spotify-this-song") {
+    
+}
+if (command==="movie-this") {
+    
+}
+if (command==="do-what-it-says") {
+    
+}
